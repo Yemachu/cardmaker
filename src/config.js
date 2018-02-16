58da5dbf52d94eda0c0fc8352258cdf993655564
Yemachu <yemachu@gmail.com>
2018-02-16 16:09:04 +0100

    Added a config file for "Require.js".

---
+++ src/config.js
@@ -0,0 +1,10 @@
+/**
+ * This file serves as a config for Require.js. It can be used to shorten
+ * pathnames, and provide alternate sources for a file (among other things).
+ * See: http://www.requirejs.org/docs/api.html#config
+ */
+var require = {
+	paths: {
+		"webfont": ["lib/webfont/webfontloader.js"]
+	}
+}
