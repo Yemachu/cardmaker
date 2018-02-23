c1d2884ebacd871250de5334a9c39a5217e137f3
Yemachu <yemachu@gmail.com>
2018-02-23 16:06:03 +0100

    Added feedback on the state of the card maker.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-define(["react", "react-dom", "App"],function(React, ReactDOM, App)
+(function ()
 {
-	ReactDOM.render(
-		React.createElement(App), 
-		document.getElementById("react-root")
-	);
-});
+	// Let the user know the application functions, but is busy with loading.
+	var root = document.getElementById("react-root");
+	root.innerText = "Loading";
+	
+	// Let the user know the application didn't load.
+	requirejs.onError = function(error)
+	{
+		root.innerText = "Loading failed";
+	};
+	
+	define(["react", "react-dom", "App"],function main(React, ReactDOM, App)
+	{
+		// Start the application.
+		ReactDOM.render(React.createElement(App), root);
+	});
+})();
