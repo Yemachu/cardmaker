779df9cb812b2baab3a8e600f249381c8d4d9a6d
Yemachu <yemachu@gmail.com>
2018-03-11 17:21:09 +0100

    Component which draws the pendulum effect and scales.

---
+++ src/tcg/ygo/layout/component/Pendulum.js
@@ -0,0 +1,69 @@
+define(["react", "react-class", "draw/Group", "draw/Text"], function Pendulum(React, ReactClass, Group, Text)
+{
+	var Pendulum = ReactClass({
+		render: function render()
+		{
+			return this.props.enabled ? React.createElement(
+				Group, 
+				{ canvas: this.props.canvas, repaint: this.props.repaint },
+				React.createElement(
+					Text, 
+					{ 
+						text: this.props.effect,
+						style: {
+							fontFamily: ["Buenard", "serif"],
+						
+							fontSize: 12,
+							textAlign: "justify",
+						
+							left: 65,
+							top: 385,
+							width: 290,
+							height: 70
+						}
+					}
+				),
+				React.createElement(
+					Text, 
+					{
+						text: this.props.blue,
+						style: {
+							fontFamily: ["Buenard", "serif"],
+							fontSize: 28,
+							textAlign: "center",
+						
+							left: 32,
+							top: 410,
+							width: 23,
+							height: 30
+						}
+					}
+				),
+				React.createElement(
+					Text,
+					{
+						text: this.props.red,
+						style: {
+							fontFamily: ["Buenard", "serif"],
+							fontSize: 28,
+							textAlign: "center",
+							
+							left: 365,
+							top: 410,
+							width: 23,
+							height: 30
+						}
+					}
+				)
+			) : null;
+		}
+	});
+	Pendulum.displayName = "Pendulum";
+	Pendulum.defaultProperties = {
+		enabled: false,
+		blue: 0,
+		red: 0,
+		effect: ""
+	};
+	return Pendulum;
+});
