6e4f3f05ede7f6185f0f3d1b4bd6338d5c0a5dbe
Yemachu <yemachu@gmail.com>
2018-03-11 17:25:53 +0100

    Component which creates a text box for the card's effect.
    It becomes bigger if it is marked as "Backrow", as this indicates the
    type and ATK/DEF are not present.

---
+++ src/tcg/ygo/layout/component/Effect.js
@@ -0,0 +1,52 @@
+define(["react", "react-class", "draw/Text"], function Effect(React, ReactClass, Text)
+{
+	var styles = {
+		Monster: {
+			fontFamily: ["Buenard", "serif"],
+			
+			fontSize: 12,
+			textAlign: "justify",
+			
+			left: 35,
+			top: 475,
+			width: 350,
+			height: 80
+		},
+		Backrow: {
+				fontFamily: ["Buenard", "serif"],
+				
+				fontSize: 12,
+				textAlign: "justify",
+				
+				left: 35,
+				top: 460,
+				width: 350,
+				height: 110
+		}
+	}
+	
+	var Effect = ReactClass({
+		render: function render()
+		{
+			return React.createElement(
+				Text, 
+				{
+					text: this.props.value, 
+					style: Object.assign({}, styles[this.props.type]),
+					repaint: this.props.repaint,
+					canvas: this.props.canvas
+				}
+			);
+		}
+	});
+	Effect.displayName = "Effect";
+	Effect.defaultProps = {
+		value: "",
+		// Affects the positioning; backrow gets more room since it does not have
+		// to accomodate for the ATK / DEF values among other things.
+		// Pendulum effects are slightly tighter.
+		type: "Monster", 
+		flavour: false,
+	};
+	return Effect;
+});
