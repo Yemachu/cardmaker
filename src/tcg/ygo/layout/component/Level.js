e0767b2f05d962946b50e6721bac528f890ed5a1
Yemachu <yemachu@gmail.com>
2018-04-17 12:06:38 +0200

    Updated the way levels are rendered; it differs for different values.

--- src/tcg/ygo/layout/component/Level.js
+++ src/tcg/ygo/layout/component/Level.js
@@ -3,16 +3,61 @@ define(["react", "react-class", "../../Stars", "draw/Group", "draw/Image"], func
 	// Maximum number of stars to draw at a time. Should not be 0.
 	var MAX_LEVEL = 12;
 	
+	
+	var styleForLevel_1_through_11 = {
+		float: "right",
+		
+		// Positioning based on 420 × 610.
+		left: 43, // (420 - width) / 2
+		top: 73, // Measured from the image.
+		
+		width: 28, // Width of a star.
+		height: 28, // Height of a star.
+		maxWidth: 334, // Max width of the total number of stars.
+		
+	};
+	var styleForLevel_12 = {
+		float: "right",
+		
+		// Positioning based on 420 × 610.
+		left: 35, // (420 - width) / 2
+		top: 73, // Measured from the image.
+		
+		width: 28, // Width of a star.
+		height: 28, // Height of a star.
+		maxWidth: 350, // Max width of the total number of stars.
+		
+	};
+	
+	var LevelStrategy = [
+		null, // Irrelevant, as no
+		styleForLevel_1_through_11, //  1
+		styleForLevel_1_through_11, //  2
+		styleForLevel_1_through_11, //  3
+		styleForLevel_1_through_11, //  4
+		styleForLevel_1_through_11, //  5
+		styleForLevel_1_through_11, //  6
+		styleForLevel_1_through_11, //  7
+		styleForLevel_1_through_11, //  8
+		styleForLevel_1_through_11, //  9
+		styleForLevel_1_through_11, // 10
+		styleForLevel_1_through_11, // 11
+		styleForLevel_12 // 12
+	];
+	
+	// Calculate the spacing between each star.
+	var spacing = ((styleForLevel_12.maxWidth - (MAX_LEVEL * styleForLevel_12.width)) / MAX_LEVEL);
+	
 	var Level = ReactClass({
 		render: function render()
 		{
 			// Contains the images which will eventually be drawn.
 			var levels = [];
-			var style = this.props.style;
-			
-			// Calculate the spacing between each star.
-			var spacing = ((style.maxWidth - (MAX_LEVEL * style.width)) / MAX_LEVEL);
 			var level = Math.min(Math.abs(this.props.value), MAX_LEVEL);
+			
+			var style = LevelStrategy[level] || {};
+			
+			
 			var rtl = this.props.value > 0;
 			
 			var width = rtl ? (style.maxWidth - (style.width + spacing)) : 0;
@@ -28,8 +73,7 @@ define(["react", "react-class", "../../Stars", "draw/Group", "draw/Image"], func
 						src: Stars[this.props.star].url,
 						// React requires identifiers when assigning children by an array.
 						key: "star:" + i,
-						style: 
-						{
+						style: {
 							// Position the star based on its number.
 							left: style.left + width + (direction * (i * (style.width +spacing))),
 							top: style.top,
@@ -38,6 +82,7 @@ define(["react", "react-class", "../../Stars", "draw/Group", "draw/Image"], func
 							height: style.height,
 						}
 					});
+					
 			}
 			return React.createElement(
 				Group,
@@ -50,18 +95,6 @@ define(["react", "react-class", "../../Stars", "draw/Group", "draw/Image"], func
 	Level.displayName = "Level";
 	Level.defaultProps = {
 		
-		style: {
-			float: "right",
-			
-			// Positioning based on 420 × 610.
-			left: 35, // (420 - width) / 2
-			top: 73, // Measured from the image.
-			
-			width: 28, // Width of a star.
-			height: 28, // Height of a star.
-			maxWidth: 350, // Max width of the total number of stars.
-			
-		}
 	};
 	return Level;
 });
