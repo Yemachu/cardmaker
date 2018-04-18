f8882fc39bdf14f9da8609927780437c3416d169
Yemachu <yemachu@gmail.com>
2018-04-18 11:12:05 +0200

    Clicking on the canvas prompts a download of the image.

--- src/draw/Canvas.js
+++ src/draw/Canvas.js
@@ -18,6 +18,7 @@ define(["react", "react-class", "./Group"], function Canvas(React, ReactClass, G
 			return React.createElement(
 				"canvas",
 				{
+					onClick: this.save.bind(this),
 					className: this.props.className,
 					width: this.props.width,
 					height: this.props.height,
@@ -25,6 +26,28 @@ define(["react", "react-class", "./Group"], function Canvas(React, ReactClass, G
 				},
 				React.createElement(Group, { canvas: this.state.canvas }, this.props.children)
 			);
+		},
+		
+		save: function save()
+		{
+			var data = "";
+			if (this.state.canvas !== null)
+			{
+				data = this.state.canvas.toDataURL();
+			}
+			var link = document.createElement("a");
+			link.setAttribute("href", data);
+			link.setAttribute("download", "Image.png");
+			if (document.createEvent)
+			{
+				var evt = document.createEvent("MouseEvent");
+				evt.initEvent("click", true, true);
+				link.dispatchEvent(evt);
+			}
+			else
+			{
+				link.click();
+			}
 		},
 		
 		componentDidMount: function didMount()
