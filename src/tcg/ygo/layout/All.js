e561943e21f942e8df0e72a9b7a80fe921e408c3
Yemachu <yemachu@gmail.com>
2018-04-23 16:26:06 +0200

    Updated the way the different layouts are stored.

--- src/tcg/ygo/layout/All.js
+++ src/tcg/ygo/layout/All.js
@@ -13,16 +13,16 @@ define([
 ], function ygo_template_all(Normal, Effect, Ritual, Fusion, Synchro, DarkSynchro, Xyz,  Link, Token, Spell, Trap)
 {
 	return {
-		Normal: Normal,
-		Effect: Effect,
-		Ritual: Ritual,
-		Fusion: Fusion,
-		Synchro: Synchro, 
-		DarkSynchro: DarkSynchro,
-		Xyz: Xyz,
-		Link: Link,
-		Token: Token,
-		Spell: Spell,
-		Trap: Trap
+		Normal: { value: "Normal", fn: Normal },
+		Effect: { value: "Effect", fn: Effect },
+		Ritual: { value: "Ritual", fn: Ritual },
+		Fusion: { value: "Fusion", fn: Fusion },
+		Synchro: { value: "Synchro", fn:Synchro }, 
+		DarkSynchro: { value: "DarkSynchro", name: "Dark Synchro", fn: DarkSynchro },
+		Xyz: { value: "Xyz", fn: Xyz },
+		Link: { value: "Link", fn: Link },
+		Token: { value: "Token", fn: Token },
+		Spell: { value: "Spell", fn: Spell },
+		Trap: { value: "Trap", fn: Trap }
 	};
 });
