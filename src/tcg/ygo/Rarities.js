ee7af22e5967155284c971c669cb8ca203d103ac
Yemachu <yemachu@gmail.com>
2018-04-23 18:42:32 +0200

    Added support for different colored card name based on rarity.

--- src/tcg/ygo/Rarities.js
+++ src/tcg/ygo/Rarities.js
@@ -2,8 +2,8 @@ define([], function()
 {
 	var path = ["res", "tcg", "ygo", "foil"].join("/");
 	return {
-		common:     { name: "Common", foil: undefined },
-		rare:       { name: "Rare", foil: undefined},
-		secret: { name: "Secret rare", foil: [ path, "Secret.png" ].join("/") }
+		common:     { name: "Common", foil: undefined, color: undefined },
+		rare:       { name: "Rare", foil: undefined, color: "silver"},
+		secret: { name: "Secret rare", foil: [ path, "Secret.png" ].join("/"), color: "silver" }
 	};
 });
--- src/tcg/ygo/layout/component/CardName.js
+++ src/tcg/ygo/layout/component/CardName.js
@@ -1,5 +1,6 @@
-define(["react", "react-class", "draw/Text"], function CardName(React, ReactClass, Text)
+define(["react", "react-class", "draw/Text", "../../Rarities"], function CardName(React, ReactClass, Text, Rarities)
 {
+	
 	var style = {
 			fontFamily: ["Spectral SC", "serif"],
 			fontSize: 32,
@@ -13,23 +14,42 @@ define(["react", "react-class", "draw/Text"], function CardName(React, ReactClas
 			width: 315,
 			height: 48,
 	}
+	var colors = {
+		default: { highlight: { color: "transparent" }, base: { color: "#000"} },
+		white: { highlight: { color: "transparent" }, base: { color: "#FFF"} },
+		silver: { highlight: { color: "#FFF" }, base: { color: "#222"} }
+	}
+	
 	var CardName = ReactClass({
 		render: function render()
 		{
+			var color = (Rarities[this.props.rarity] || {}).color || this.props.color;
+			
 			return React.createElement(
-				Text, 
+				React.Fragment,
+				null,
+				React.createElement(Text, 
 				{
 					text: this.props.value, 
-					style: Object.assign({}, style, { color: this.props.color }),
+					style: Object.assign({}, style, (colors[color] || colors.default).highlight, { top: style.top+1 }),
 					repaint: this.props.repaint,
 					canvas: this.props.canvas
-				}
+				}),
+				React.createElement(Text,
+					{
+						text: this.props.value,
+						style: Object.assign({}, style, (colors[color] || colors.default).base),
+						repaint: this.props.repaint,
+						canvas: this.props.canvas
+					}
+				)
 			);
 		}
 	});
 	CardName.displayName = "CardName";
 	CardName.defaultProps = {
 		value: "",
+		rarity: "common",
 		color: "black"
 	};
 	return CardName;
