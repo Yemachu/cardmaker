70657a13cf3bf99a1aec8e18812274fce2048685
Yemachu <yemachu@gmail.com>
2018-06-27 14:17:17 +0200

    Initial setup for using the handlebars templating engine.

--- src/tcg/CardMaker.js
+++ src/tcg/CardMaker.js
@@ -1,7 +1,29 @@
-define(["text!./ygo/Layout.html"], function (layout)
+define(["handlebars", "text!./ygo/Layout.hbs", "text!./ygo/UI.html"], function (handlebars, layout, ui)
 {
+	
 	return function CardMaker(root)
 	{
-		root.appendChild(document.createElement("iframe")).setAttribute("src", "src/tcg/ygo/Layout.html");
+		var iframe = root.appendChild(document.createElement("iframe"));
+		console.log(handlebars)
+		var template = handlebars.compile(layout);
+		iframe.setAttribute("src", "about:blank");
+		iframe.width  = 420;
+		iframe.height = 610;
+		iframe.addEventListener("load", function()
+		{
+			var document = iframe.contentDocument || iframe.contentWindow.document;
+			document.open();
+			document.write(template({ card: 
+				{ 
+					name: "Black Chicken", 
+					effect: "This card cannot be used as a material for a summon.",
+					pendulum: { enabled: false, blue: 10 },
+					atk: 2500
+				}}));
+			document.close();
+		})
+		root.appendChild(document.createElement("div")).innerHTML = ui;
+		
+		
 	}
 });
