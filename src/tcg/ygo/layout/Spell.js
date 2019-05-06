7d8a4262bbbcf2777d2e4cc5c8d9a84c192a7a01
Yemachu <yemachu@gmail.com>
2019-05-06 13:26:52 +0200

    Added pendulum support for Spell, Trap, and Link cards.

--- src/tcg/ygo/layout/Spell.js
+++ src/tcg/ygo/layout/Spell.js
@@ -6,23 +6,25 @@ define(["react", "react-class", "draw/Group", "./component/All"], function Spell
 			return React.createElement(
 				Group,
 				this.props,
-				React.createElement(C.Image, { value: this.props.image, rarity: this.props.rarity }),
-				React.createElement(C.Border, { value: "Spell" }),
+				React.createElement(C.Image, { value: this.props.image, pendulum: this.props.pendulum.enabled, rarity: this.props.rarity }),
+				React.createElement(C.Border, { value: "Spell", pendulum: this.props.pendulum.enabled }),
 				React.createElement(C.CardName, { value: this.props.name, color: "white", rarity: this.props.rarity }),
 				React.createElement(C.Attribute, { value: this.props.attribute }),
-				
+
+				React.createElement(C.Pendulum, this.props.pendulum),
+
 				React.createElement(C.Type, { value: this.props.type, type: "Backrow", icon: this.props.icon }),
 				React.createElement(C.Effect, { value: this.props.effect, type: "Backrow" }),
-				
+
 				React.createElement(C.Serial, { value: this.props.serial }),
-				React.createElement(C.Id, { value: this.props.id, position: "regular" }),
+				React.createElement(C.Id, { value: this.props.id, position: this.props.pendulum.enabled ? "pendulum" : "regular"}),
 				React.createElement(C.Copyright, { value: this.props.copyright })
 			);
 		}
 	});
 	Spell.displayName = "Spell";
 	Spell.defaultProps = {
-		
+
 	};
 	return Spell;
 });
